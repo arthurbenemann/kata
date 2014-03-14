6f37eb12440ca18badd64ddd96c55f83b437c3cc
Arthur Benemann <arthur.benemann@gmail.com>
2014-03-14 04:02:17 -0300

    Added option to chose where the particles are generated, and a finish condition.

--- crystal.c
+++ crystal.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZEX 	320
-#define SIZEY 	160
+#define SIZEX 	500
+#define SIZEY 	250
 #define STEP 	4
 
-#define FRAME_SKIP 100000
+#define FRAME_SKIP 500000
+
+#define CREATE_FROM_EDGE
 
 typedef struct{
 	int x,y;
@@ -14,36 +16,33 @@ typedef struct{
 int m[SIZEX][SIZEY];
 point particle;
 
-
-int frame;
 void plot(point particle){
-	if(frame++>=FRAME_SKIP){
-		frame = 0;
-		printf("\033[2J\033[1;1H");
+	printf("\033[2J\033[1;1H");
 
-		int i,j;
-		for(i=0;i<SIZEY;i++){
-			for(j=0;j<SIZEX;j++){
-				if(m[j][i]){
-					printf("#");
-				}else if((particle.x==j) & (particle.y==i)){
-					printf("*");
-				}else
-					printf(" ");
-			}
-			printf("\n");
+	int i,j;
+	for(i=0;i<SIZEY;i++){
+		for(j=0;j<SIZEX;j++){
+			if(m[j][i]){
+				printf("#");
+			}else if((particle.x==j) & (particle.y==i)){
+				printf("*");
+			}else
+				printf(" ");
 		}
+		printf("\n");
 	}
 }
 
 void create(){
+#ifndef CREATE_FROM_EDGE
 	// Create a new particle
 	particle.x = rand()%SIZEX;
 	particle.y = rand()%SIZEY;
-
-	/* // Create a new particle at the boarder
+#else
+	// Create a new particle at the boarder
 	particle.x = (rand()%2)*(SIZEX-1);
-	particle.y = (rand()%2)*(SIZEY-1);*/
+	particle.y = (rand()%2)*(SIZEY-1);
+#endif
 }
 
 int isValid(){
@@ -84,6 +83,23 @@ void randomWalk(){
 	//printf("particle at x:%d y:%d %d",particle.x,particle.y,touch());
 }
 
+int isFilled(){
+	int i,j;
+	for(i=0;i<SIZEY;i++){
+		if(m[0][i])
+			return 1;
+		if(m[SIZEX-1][i])
+			return 1;
+	}
+	for(j=0;j<SIZEX;j++){	
+		if(m[j][0])
+			return 1;
+		if(m[j][SIZEY-1])
+			return 1;
+	}
+	return 0;
+}
+
 void main(){
 
 	// Place initial particle
@@ -95,9 +111,14 @@ void main(){
 
 	create();	
 
-	while(1){
+	int frame;
+	long count=0;
+	while(!isFilled()){
 		// Plot
-		plot(particle);
+		if(frame++>=FRAME_SKIP){
+			frame = 0;
+			plot(particle);
+		}
 		
 		if(touch()){
 			solidify();
@@ -108,12 +129,11 @@ void main(){
 		if(!isValid()){
 			create();
 		}
-
-
-		char trash;
-
-		//scanf("%c",&trash);
+		
+		count++;
 	}
-	
-	
+
+	plot(particle);
+	printf("Finished after %ld iterations \n",count);
+
 }
